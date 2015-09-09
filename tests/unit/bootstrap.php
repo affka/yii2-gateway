48e3cd67ec9c069facdfe5be93820914cbe5bc3a
Kozhin Vladimir <affka@affka.ru>
2015-09-09 08:11:33 +0700

    test run fix

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -7,8 +7,6 @@ $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 // require composer autoloader if available
-require_once(__DIR__ . '/../../../../vendor/autoload.php');
-require_once(__DIR__ . '/../../../../vendor/yiisoft/yii2/Yii.php');
+require_once(__DIR__ . '/../../vendor/autoload.php');
+require_once(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
-Yii::setAlias('@app', __DIR__ . '/../../../../app');
-Yii::setAlias('@yiiunit', __DIR__);
